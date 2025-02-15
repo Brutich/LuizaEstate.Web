0b9017b1defbb063a3bef6701ecd0049570d0719
Oleg Brutich <oleg87arh@gmail.com>
2025-02-15 23:31:03 +0300

    Customize theme

--- Layout/MainLayout.razor.cs
+++ Layout/MainLayout.razor.cs
@@ -4,8 +4,18 @@ namespace LuizaEstate.Web.Layout;
 
 public partial class MainLayout
 {
+    private static readonly PaletteLight paletteLight =
+        new()
+        {
+            Dark = "#2E4942",
+            TextPrimary = "#2E4942",
+            Primary = "#B87E58",
+
+        };
+
     private readonly MudTheme mainTheme = new MudTheme()
     {
+        PaletteLight = paletteLight,
         Typography = new Typography()
         {
             Default = new DefaultTypography()
